da7f38a45cafac1c68f741287d4239917b514904
dada <dadad@gmail.com>
2025-01-15 20:45:02 +0100

    Initial Commit

---
+++ FireBasePrueba/AuthViewController.swift
@@ -0,0 +1,127 @@
+//
+//  AuthViewController.swift
+//  FireBasePrueba
+//
+//  Created by Tardes on 10/1/25.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseAnalytics
+import GoogleSignIn
+import FirebaseRemoteConfig
+
+class AuthViewController: UIViewController {
+
+    @IBOutlet weak var LoginButton: UIButton!
+    @IBOutlet weak var CreateButton: UIButton!
+    @IBOutlet weak var UserPassword: UITextField!
+    @IBOutlet weak var UserEmail: UITextField!
+    
+    @IBAction func SignByGooglePress(_ sender: Any) {
+        
+        // Configure Google SignIn with Firebase
+                // Start the sign in flow!
+                GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+                    guard error == nil else {
+                        return
+                    }
+
+                    guard let user = result?.user, let idToken = user.idToken?.tokenString else {
+                        return
+                    }
+
+                    let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+
+                    Auth.auth().signIn(with: credential) { result, error in
+                        guard error == nil else {
+                            return
+                        }
+                        
+                        // At this point, our user is signed in
+                        self.performSegue(withIdentifier: "goToHome", sender: nil)
+                    }
+                }
+        
+    }
+    
+    
+    
+    @IBAction func forgotPassword(_ sender: Any) {
+        let username = UserEmail.text!
+                Auth.auth().sendPasswordReset(withEmail: username) { error in
+                    if (error != nil) {
+                        print(error!.localizedDescription)
+                    }
+                }
+                let alert = UIAlertController(title: "Recuperar contraseña", message: "Te hemos enviado un correo a \(username) para recuperar tu contraseña.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default))
+                self.present(alert, animated: true)
+    }
+    
+    
+    @IBAction func CreatePressed(_ sender: UIButton) {
+        
+        if let email = UserEmail.text,let password =
+            UserPassword.text{
+            
+            Auth.auth().createUser(withEmail: email, password: password){
+                (result,error)in
+                if let result = result , error == nil{
+                    self.navigationController?
+                        .pushViewController(HomeViewController(email: result.user.email!, provider: .basic), animated: true)
+                    
+                }else{
+                    let alertController = UIAlertController(title: "Error", message: "Se ha producido un error registrando al usuario", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    
+                    self.present(alertController, animated: true,completion: nil)
+                }
+                
+            }
+            
+        }
+    }
+    @IBAction func LoginPressed(_ sender: Any) {
+        
+        if let email = UserEmail.text,let password =
+            UserPassword.text{
+            
+            Auth.auth().signIn(withEmail: email, password: password){
+                (result,error)in
+                if let result = result , error == nil{
+                    self.navigationController?
+                        .pushViewController(HomeViewController(email: result.user.email!, provider: .basic), animated: true)
+                    
+                }else{
+                    let alertController = UIAlertController(title: "Error", message: "Se ha producido un error registrando al usuario", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    
+                    self.present(alertController, animated: true,completion: nil)
+                }
+                
+            }
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Authenticaton"
+        
+        Analytics.logEvent("InitScreen", parameters: ["Message":"Integracion de FireBase Completa"])
+        
+        //Remote Config
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 60 //Tiempo que se tarda en hacer el cambio, de serie son 12 horas
+        
+        let remoteConfig = RemoteConfig.remoteConfig()
+        remoteConfig.configSettings = settings
+        remoteConfig.setDefaults(["show_error_button":NSNumber(true),"error_button_text":NSString("Forzar error")])
+        
+    }
+
+
+}
+
---
+++ FireBasePrueba/HomeViewController.swift
@@ -0,0 +1,94 @@
+//
+//  HomeViewController.swift
+//  FireBasePrueba
+//
+//  Created by Tardes on 10/1/25.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseRemoteConfig
+
+enum ProviderType:String{
+    case basic
+    case google
+}
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var UserLabel: UILabel!
+    @IBOutlet weak var PasswordLabel: UILabel!
+    @IBOutlet weak var CloseButton: UIButton!
+    
+<<<<<<< HEAD
+    private let email:String
+    private let provider:ProviderType
+    
+=======
+    @IBOutlet weak var ErrorButton: UIButton!
+    var email:String = ""
+    var provider:ProviderType = ProviderType.basic
+>>>>>>> 8aba4c7 (Integrados crashlytics y remoteConfig)
+    
+    init(email:String, provider:ProviderType){
+        self.email = email
+        self.provider = provider
+        super.init(nibName: "HomeViewController", bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UserLabel.text = email
+        PasswordLabel.text = provider.rawValue
+
+        // Do any additional setup after loading the view.
+        
+        // Remote Config
+        let remoteConfig = RemoteConfig.remoteConfig()
+        remoteConfig.fetchAndActivate{(status , error) in
+            if status != .error{
+                let showErrorButton = remoteConfig.configValue(forKey: "show_error_button").boolValue
+                let errorButtonText = remoteConfig.configValue(forKey: "error_button_text").stringValue
+                
+                //El pintado de la app se tiene que realizar en este hilo
+                DispatchQueue.main.async {
+                    self.ErrorButton.setTitle(errorButtonText, for: .normal)
+                    self.ErrorButton.isHidden = !showErrorButton
+                }
+                
+            }}
+    }
+    
+    @IBAction func CloseSession(_ sender: Any) {
+        
+        switch provider{
+            
+        case .basic, .google:
+            do{
+                try Auth.auth().signOut()
+                navigationController?.popViewController(animated: true)
+            }catch{
+                
+            }
+        }
+    }
+    @IBAction func FatalErrorPressed(_ sender: Any) {
+        
+        fatalError()
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
